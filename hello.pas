17575efc5fb735fc385061b582c82059c6526bbb
Тимофей Базолий <tim.23tim23.23@gmail.com>
2024-10-13 20:28:20 +0800

    hello.pas was added

---
+++ hello.pas
@@ -0,0 +1,13 @@
+﻿{Измените код в файле hello.pas, чтобы программа спрашивала у пользователя язык, 
+на котором он говорит и выводила приветствие на этом языке.}
+
+begin
+  var n_lng := ReadInteger('Change your language: 1-English, 2-Русский, 3-日本語:');
+  Assert(n_lng in 1..3);
+  
+  case n_lng of
+    1: Println('Hello!');
+    2: Println('Приветсвую!');
+    3: Println('こんにちは!');
+  end;
+end.
